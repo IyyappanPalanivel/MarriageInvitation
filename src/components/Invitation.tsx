08c8710bbfea1ff9ccaf11df9a0e2d59f33abdf0
IyyappanPalanivel <iyyappan.p@technotackle.com>
2024-02-22 22:48:20 +0530

    - Invitation image added - Invitation video added - Navbar optimise done

--- src/components/Invitation.tsx
+++ src/components/Invitation.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import tulip_bg from '../assets/tulip_bg.jpg';
+import invitation from '../assets/invitation.jpg';
+import RamkumarMarriageInvite from "../assets/videos/RamkumarMarriageInvite.mp4";
 
 const Invitation = () => {
     return (
         <div
-            className="max-w-screen-2xl flex min-h-screen bg-cover"
+            className="max-w-screen-2xl flex min-h-screen bg-cover justify-center items-center"
             style={{
                 backgroundImage: `url(${tulip_bg})`,
             }}
         >
-            <div className='px-4 lg:px-14 py-44'>
-                <div className='bg-neutralWhite px-4 py5 rounded'>
-                    <h5>Together with Our Families</h5>
+            <div className='px-4 lg:px-14 py-44 flex justify-around w-full flex-col md:flex-row items-center gap-8'>
+                {/* Invitation Image */}
+                <div className='shadow-md bg-white'>
+                    <img src={invitation} width="400" height="300"/>
+                </div>
+                {/* Video */}
+                <div className='shadow-md bg-white'>
+                    <video preload="auto" width="400" height="300" controls autoPlay loop>
+                        <source src={RamkumarMarriageInvite} type="video/mp4" />
+                        Your browser does not support HTML5 video.
+                    </video>
                 </div>
             </div>
+
+
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderBanner from '../assets/HeaderBanner.png'
 
 const Navbar = () => {
+
+    const [isSticky, setIsSticky] = useState(false);
+
+    useEffect(()=>{
+        const handleScroll = () => {
+            if(window.scrollY > 100){
+                setIsSticky(true);
+            }
+            else{
+                setIsSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.addEventListener('scroll', handleScroll);
+        }
+    })
+
     return (
-        <div className='w-full items-center justify-center p-4 shadow-md fixed left-0 right-0 top-0 mx-auto'>
+        <div className={`w-full items-center justify-center p-4 shadow-md fixed left-0 right-0 top-0 mx-auto ${isSticky ? "sticky bg-white" : ""}`}>
             <img className='h-14' src={HeaderBanner} alt='logo'/>
         </div>
     );
