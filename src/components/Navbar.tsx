0d5be57e42445c054cbce1065d80382f417443d2
IyyappanPalanivel <iyyappan.p@technotackle.com>
2024-02-22 10:59:08 +0530

    - Header path changed without src in reference name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HeaderBanner from '../assets/HeaderBanner.png'
 
 const Navbar = () => {
     return (
         <div className='w-full items-center justify-center p-4 shadow-md fixed left-0 right-0 top-0 mx-auto'>
-            <img className='h-14' src='/src/assets/HeaderBanner.png' alt='logo'/>
+            <img className='h-14' src={HeaderBanner} alt='logo'/>
         </div>
     );
 };
