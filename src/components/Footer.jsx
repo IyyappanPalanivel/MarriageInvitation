b2a2ea375e1cce3e6088d4a654cacf4aeb7c3fef
IyyappanPalanivel <iyyappan.p@technotackle.com>
2024-09-03 23:48:17 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
 
     return (
         <div className='w-full bg-neutralBlue py-8 flex-col items-center text-center'>
-            <p className='italic'> © 2024 |  <a className='underline text-neutralWhite left-1'>Grey Matrix</a>
-            </p>
+            {/* <p className='italic'> © 2024 |  <a className='underline text-neutralWhite left-1'>Grey Matrix</a>
+            </p> */}
             {/* Grey Matrix */}
             <div className='items-center justify-center text-center' onClick={handleGreyMatrixClick}>
                 <div className='flex items-center justify-center mt-1 md:mt'>
